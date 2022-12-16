afb687bccc572699a8105b293c1c48940fea1d74
Veniamin Viflyantsev <knok16@gmail.com>
2022-12-16 21:45:05 +0400

    Day 16 task 2 solution

--- src/main/kotlin/day16/Solution.kt
+++ src/main/kotlin/day16/Solution.kt
@@ -2,6 +2,7 @@ package day16
 
 import onBits
 import java.io.File
+import java.util.*
 import kotlin.math.max
 
 data class Valve(
@@ -10,7 +11,7 @@ data class Valve(
     val edgesTo: Set<String>
 )
 
-fun solve(valves: List<Valve>, initialValve: String, time: Int): Long {
+fun task1(valves: List<Valve>, initialValve: String, time: Int): Long {
     val valves = valves.sortedByDescending { it.rate }
     val edges = valves.map { valve ->
         valve.edgesTo.map { id -> valves.indexOfFirst { it.id == id } }
@@ -21,8 +22,8 @@ fun solve(valves: List<Valve>, initialValve: String, time: Int): Long {
         valves.slice(mask.onBits()).sumOf { it.rate }
     }
     val dp = Array(time + 1) { Array(valves.size) { LongArray(maskSize) { -1 } } }
-    fun update(time: Int, ends: Int, mask: Int, value: Long) {
-        dp[time][ends][mask] = max(dp[time][ends][mask], value)
+    fun update(time: Int, position: Int, mask: Int, value: Long) {
+        dp[time][position][mask] = max(dp[time][position][mask], value)
     }
 
     update(0, valves.indexOfFirst { it.id == initialValve }, 0, 0)
@@ -50,6 +51,68 @@ fun solve(valves: List<Valve>, initialValve: String, time: Int): Long {
     return dp[time].maxOf { it.max() }
 }
 
+fun task2(valves: List<Valve>, initialValve: String, time: Int): Long {
+    val valves = valves.sortedByDescending { it.rate }
+    val edges = valves.map { valve ->
+        valve.edgesTo.map { id -> valves.indexOfFirst { it.id == id } }
+    }
+
+    val maskSize = 1 shl valves.count { it.rate > 0 }
+    val sums = LongArray(maskSize) { mask ->
+        valves.slice(mask.onBits()).sumOf { it.rate }
+    }
+    var dpNext = Array(valves.size) { Array(valves.size) { LongArray(maskSize) } }
+    fun update(myPosition: Int, elephantPosition: Int, mask: Int, value: Long) {
+        dpNext[myPosition][elephantPosition][mask] = max(dpNext[myPosition][elephantPosition][mask], value)
+    }
+
+    val initialPosition = valves.indexOfFirst { it.id == initialValve }
+    var dp = Array(valves.size) { Array(valves.size) { LongArray(maskSize) { -1 } } }
+    dp[initialPosition][initialPosition][0] = 0
+
+    data class Move(
+        val newPosition: Int,
+        val newMask: Int
+    )
+
+    fun movesFrom(position: Int, mask: Int): List<Move> =
+        edges[position].map { Move(it, mask) } + Move(position, mask) + if (valves[position].rate > 0) {
+            listOf(Move(position, mask or (1 shl position)))
+        } else emptyList()
+
+    for (t in 0 until time) {
+        dpNext.forEach { it.forEach { Arrays.fill(it, -1) } }
+
+        for (myPosition in valves.indices) {
+            for (elephantPosition in valves.indices) {
+                dp[myPosition][elephantPosition].forEachIndexed { mask, value ->
+                    if (value >= 0) {
+                        val nextValue = value + sums[mask]
+                        val myMoves = movesFrom(myPosition, mask)
+                        val elephantMoves = movesFrom(elephantPosition, mask)
+                        for (myMove in myMoves) {
+                            for (elephantMove in elephantMoves) {
+                                update(
+                                    myMove.newPosition,
+                                    elephantMove.newPosition,
+                                    myMove.newMask or elephantMove.newMask,
+                                    nextValue
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        val tmp = dp
+        dp = dpNext
+        dpNext = tmp
+    }
+
+    return dp.maxOf { it.maxOf { it.max() } }
+}
+
 fun main() {
     val regex = Regex("^Valve ([A-Z]+) has flow rate=(\\d+); tunnels? leads? to valves? (.+)$")
     listOf(
@@ -66,8 +129,12 @@ fun main() {
                 Valve(id, rate.toLong(), edges.split(", ").toSet())
             }
 
-        val task1 = solve(valves, "AA", 30)
+        val task1 = task1(valves, "AA", 30)
 
-        println(task1)
+        println(task1) // 2183
+
+        val task2 = task2(valves, "AA", 26)
+
+        println(task2) // 2911
     }
 }
