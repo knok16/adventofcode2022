6a7a07e1a2b96a72fc6bd13b3036afa4ad04b0ac
Veniamin Viflyantsev <knok16@gmail.com>
2022-12-18 19:02:05 +0400

    Day 18 Task 2

--- src/main/kotlin/day18/Solution.kt
+++ src/main/kotlin/day18/Solution.kt
@@ -8,6 +8,44 @@ data class Voxel(
     val z: Int
 )
 
+fun Voxel.neighbours(): List<Voxel> = listOf(
+    Voxel(x - 1, y, z),
+    Voxel(x + 1, y, z),
+    Voxel(x, y - 1, z),
+    Voxel(x, y + 1, z),
+    Voxel(x, y, z - 1),
+    Voxel(x, y, z + 1)
+)
+
+fun calculateSurfaceArea(shape: Set<Voxel>): Int = shape.sumOf { voxel ->
+    voxel.neighbours().count { it !in shape }
+}
+
+val IntRange.size
+    get() = last - first + 1
+
+fun calculateExternalSurface(shape: Set<Voxel>): Int {
+    fun <T> Set<T>.rangeOf(valueExtractor: (T) -> Int): IntRange =
+        (minOf(valueExtractor) - 1)..(maxOf(valueExtractor) + 1)
+
+    val xRange = shape.rangeOf { it.x }
+    val yRange = shape.rangeOf { it.y }
+    val zRange = shape.rangeOf { it.z }
+
+    val result = HashSet<Voxel>()
+
+    fun dfs(voxel: Voxel) {
+        if (voxel !in result && voxel !in shape && voxel.x in xRange && voxel.y in yRange && voxel.z in zRange) {
+            result.add(voxel)
+            voxel.neighbours().forEach(::dfs)
+        }
+    }
+
+    dfs(Voxel(xRange.first, yRange.first, zRange.first))
+
+    return calculateSurfaceArea(result) - 2 * (xRange.size * yRange.size + xRange.size * zRange.size + yRange.size * zRange.size)
+}
+
 fun main() {
     listOf(
         "inputs/day18.example.txt",
@@ -23,19 +61,12 @@ fun main() {
                 Voxel(x, y, z)
             }.toSet()
 
-        fun Voxel.neighbours(): List<Voxel> = listOf(
-            Voxel(x - 1, y, z),
-            Voxel(x + 1, y, z),
-            Voxel(x, y - 1, z),
-            Voxel(x, y + 1, z),
-            Voxel(x, y, z - 1),
-            Voxel(x, y, z + 1)
-        )
+        val task1 = calculateSurfaceArea(shape)
 
-        val task1 = shape.sumOf { voxel ->
-            voxel.neighbours().count { it !in shape }
-        }
+        println(task1) // 3432
 
-        println(task1)
+        val task2 = calculateExternalSurface(shape)
+
+        println(task2) // 2042
     }
 }
